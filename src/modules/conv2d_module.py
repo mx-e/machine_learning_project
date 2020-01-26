785414b7fc1c70b7c9e6795d1f18936f255154f9
Maximilian Eissler <maximilan.eissler@cct-ev.de>
2020-01-26 13:42:39 +0100

    adapted code of i2a and modules to new action space

--- src/modules/conv2d_module.py
+++ src/modules/conv2d_module.py
@@ -11,7 +11,7 @@ class Conv2d_Module(nn.Module):  # an actor-critic neural network
     def __init__(self, is_sokoban):
         super(Conv2d_Module, self).__init__()
         if(is_sokoban):
-            self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=4, stride=2, padding=1)
+            self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
             self.conv2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1)
 
         else:
--- src/modules/policy_output_module.py
+++ src/modules/policy_output_module.py
@@ -13,9 +13,10 @@ class Policy_Output_Module(nn.Module):  # an actor-critic neural network
         self.linear1 = nn.Linear(input_size, 128)
         self.linear2 = nn.Linear(128, num_action)
         self.apply(initialize_weights)
+        self.input_size = input_size
         
     def forward(self, inputs, train=True, hard=False):
         
-        inputs = inputs.view(-1, 144)
+        inputs = inputs.view(-1, self.input_size)
         x = F.relu(self.linear1(inputs))
         return F.softmax(self.linear2(x), dim=1)
